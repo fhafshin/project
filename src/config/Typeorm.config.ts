7305aa256c0aea02b07b37bb16a206c601e8ee79
fhafshin <fhafshin@gmail.com>
2024-09-06 02:08:34 +0330

    ww

--- src/config/Typeorm.config.ts
+++ src/config/Typeorm.config.ts
@@ -12,7 +12,7 @@ export function TypeOrmConfig(): TypeOrmModuleOptions {
     database: DB_NAME,
     port: +DB_PORT,
     autoLoadEntities: false,
-    synchronize: false,
+    synchronize: true,
 
     entities: ['dist/**/**/*.entity.{ts,js}', 'dist/**/**/**/*.entity.{ts,js}'],
   };

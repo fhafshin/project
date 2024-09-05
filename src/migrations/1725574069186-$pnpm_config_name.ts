311d6f295e85d62ee98655a5f831374ee978f211
fhafshin <fhafshin@gmail.com>
2024-09-06 02:04:10 +0330

    gggrr

---
+++ src/migrations/1725574069186-$pnpm_config_name.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class  $pnpmConfigName1725574069186 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
